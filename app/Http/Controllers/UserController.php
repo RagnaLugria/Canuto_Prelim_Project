003430bba422a9aaf0c501b11d2b46425fd0a564
Jay <jeremiahcanuto@yahoo.com>
2017-01-02 17:29:20 +0800

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Controller
+    
+class UserController extends Controller
+{
+    public function postSignUp(Request $request)
+    {
+        $email = $request['email'];
+        $first_name = $request['first_name'];
+        $password = bcrypt($request['password']);
+        
+        $user = new user();
+        $user->email  = $email;
+        $user->first_name = $first_name;
+        $user->password = $password;
+        
+        $user->save();
+        
+        return redirect()->back();
+    }
+    
+    public function postSignIn(Request $request)
+    {
+        
+    }
+}
