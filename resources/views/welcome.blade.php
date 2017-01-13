5b4e0b300c4b901d029f2c93e8e14cc8d62f122c
Jay <jeremiahcanuto@yahoo.com>
2017-01-13 15:16:30 +0800

    added more sign up info

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,6 +18,14 @@
                     <label form="first_name">Your First Name</label>
                     <input class="form-control" type="text" name="first_name" id="first_name" value="{{ Request::old('first_name') }}">
                 </div>
+                <div class="form-group {{ $errors->has('last_name') ? 'has-error' : '' }}">
+                    <label form="last_name">Your Last Name</label>
+                    <input class="form-control" type="text" name="last_name" id="last_name" value="{{ Request::old('last_name') }}">
+                </div>
+                <div class="form-group {{ $errors->has('phone') ? 'has-error' : '' }}">
+                    <label form="last_name">Your Phone Number</label>
+                    <input class="form-control" type="text" name="phone" id="phone" value="{{ Request::old('phone') }}">
+                </div>
                 <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
                     <label form="password">Your Password</label>
                     <input class="form-control" type="password" name="password" id="password" value="{{ Request::old('password') }}">
